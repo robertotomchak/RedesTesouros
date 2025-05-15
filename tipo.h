6c5e15d3c3d5297f861a5868adc3731615c07b6b
RobertoTomchak <robertotomchak@gmail.com>
2025-05-14 22:38:37 -0300

    Adicionando tipos de mensagens

---
+++ tipo.h
@@ -0,0 +1,18 @@
+/*
+    Define os tipos de mensagens
+*/
+
+#define TIPO_ACK 0
+#define TIPO_NACK 1
+#define TIPO_OK_ACK 2
+#define TIPO_TAM 4
+#define TIPO_DADOS 5
+#define TIPO_TEXTO_ACK 6
+#define TIPO_VIDEO_ACK 7
+#define TIPO_IMAGEM_ACK 8
+#define TIPO_FIM_ARQUIVO 9
+#define TIPO_DIREITA 10
+#define TIPO_CIMA 11
+#define TIPO_BAIXO 12
+#define TIPO_ESQUERDA 13
+#define TIPO_ERRO 15
